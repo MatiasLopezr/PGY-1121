03aca2c42e8ad276e2dc6b73226eb4ff5031dad6
MatiasLopezr <134552823+MatiasLopezr@users.noreply.github.com>
2023-05-24 16:43:13 -0400

    Datos solicitados

---
+++ CLASE-12/datos.py
@@ -0,0 +1,9 @@
+#EJEMPLO DE USO DE GITHUB
+print("INGRESO DE DATOS")
+print("----------------")
+vNom = input("Ingrese su Nombre:  ")
+vEdad = input("Ingrese su Edad:  ")
+print("--------------------------")
+print(f"su Nombre es: {vNom}")
+print(f"su Edad es: {vEdad}")
+print("programa finalizado...")
